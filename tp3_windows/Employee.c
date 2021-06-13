9a98fb6d2f7cc36ed58d823b1d90a3b95a545215
Blu2021 <juanpablodaudm@gmai.com>
2021-06-13 00:15:20 -0300

    tp 3 finalizado

---
+++ tp3_windows/Employee.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Employee.h"
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr,char* sueldo)
+{
+	Employee* punteroEmpleado;
+
+	punteroEmpleado=((Employee*)malloc(sizeof(Employee)));
+	if(punteroEmpleado!=NULL)
+	{
+		employee_setId(punteroEmpleado,atoi(idStr));
+		employee_setNombre(punteroEmpleado,nombreStr);
+		employee_setHorasTrabajadas(punteroEmpleado,atoi(horasTrabajadasStr));
+		employee_setSueldo(punteroEmpleado,atoi(sueldo));
+	}
+
+	return punteroEmpleado;
+}
+
+Employee* employee_new()
+{
+	Employee* punteroEmpleado;
+
+	punteroEmpleado=((Employee*)malloc(sizeof(Employee)));
+
+	return punteroEmpleado;
+}
+
+int employee_setId(Employee* this,int id)
+{
+	int exito=0;
+
+	if(this!=NULL)
+	{
+		exito=1;
+		(*this).id=id;
+	}
+	return exito;
+}
+
+int employee_setNombre(Employee* this,char* nombre)
+{
+	int exito=0;
+
+	if(this!=NULL)
+	{
+		exito=1;
+		strcpy((*this).nombre,nombre);
+	}
+
+	return exito;
+}
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas)
+{
+	int exito=0;
+
+	if(this!=NULL)
+	{
+		exito=1;
+		(*this).horasTrabajadas=horasTrabajadas;
+	}
+
+	return exito;
+}
+
+int employee_setSueldo(Employee* this,int sueldo)
+{
+	int exito=0;
+
+	if(this!=NULL)
+	{
+		exito=1;
+		(*this).sueldo=sueldo;
+	}
+
+	return exito;
+}
+
+int employee_getId(Employee* this,int* id)
+{
+	int exito=0;
+
+	if(this!=NULL&&id!=NULL)
+	{
+		exito=1;
+		*id=(*this).id;
+	}
+
+	return exito;
+}
+
+int employee_getNombre(Employee* this,char* nombre)
+{
+	int exito=0;
+
+	if(this!=NULL&&nombre!=NULL)
+	{
+		exito=1;
+		strcpy(nombre,(*this).nombre);
+	}
+
+	return exito;
+}
+
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
+{
+	int exito=0;
+
+	if(this!=NULL&&horasTrabajadas!=NULL)
+	{
+		exito=1;
+		*horasTrabajadas=(*this).horasTrabajadas;
+	}
+
+	return exito;
+}
+
+int employee_getSueldo(Employee* this,int* sueldo)
+{
+	int exito=0;
+
+	if(this!=NULL&&sueldo!=NULL)
+	{
+		exito=1;
+		*sueldo=(*this).sueldo;
+	}
+
+	return exito;
+}
